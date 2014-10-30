289ea8921b5d98bcdbc717feb33db68871708331
James <jameshe424@gmail.com>
2014-10-30 16:55:07 -0700

    DriveTrain stuff

--- RobotProject3/src/robotcode/subsystems/DriveTrain.java
+++ RobotProject3/src/robotcode/subsystems/DriveTrain.java
@@ -1,28 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package robotcode.subsystems;
 
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
- * @author Ashwin
+ * @author Ashwin, James, Kavya, Calvin and Emily
  */
 public class DriveTrain extends Subsystem {
+    private Talon talon1;
+    private Talon talon2;
+    private Talon talon3;
+    private Talon talon4;
+    public DriveTrain()
+    {
+        talon1 = new Talon(1);
+        talon2 = new Talon(2);
+        talon3 = new Talon(3);
+        talon4 = new Talon(4);
+    }    
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    
-    private Talon talon1 = new Talon(1);
-    private Talon talon2 = new Talon(2);
-    private Talon talon3 = new Talon(3);
-    private Talon talon4 = new Talon(4);
+    public Talon getTalon1()
+    {return talon1;}
+    public Talon getTalon2()
+    {return talon2;}
+    public Talon getTalon3()
+    {return talon3;}
+    public Talon getTalon4()
+    {return talon4;}
 }
