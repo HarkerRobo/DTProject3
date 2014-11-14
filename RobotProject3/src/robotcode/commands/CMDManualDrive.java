327e752fc2eb7b572f60f2c3429a92659239af3c
James <jameshe424@gmail.com>
2014-11-13 16:45:41 -0800

    k

--- RobotProject3/src/robotcode/commands/CMDManualDrive.java
+++ RobotProject3/src/robotcode/commands/CMDManualDrive.java
@@ -34,7 +34,7 @@ public class CMDManualDrive extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return oi.gamepad.getButtonYState();
+        return oi.gamepad.getButtonYState(); 
     }
 
     // Called once after isFinished returns true
